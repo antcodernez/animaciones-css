1819b3811d8128d1780fdc52cfcd090920b9dd6e
Antonio Jiménez <marshday5@gmail.com>
2024-02-18 18:29:38 -0600

    implementing the counter on the game

---
+++ code.js
@@ -0,0 +1,20 @@
+const bunnys = document.querySelectorAll(".box-bunny");
+const score = document.querySelector(".score-value");
+let scoreNumber = 0;
+
+bunnys.forEach(bunny => {
+    bunny.addEventListener("click", () => {
+        
+        if(scoreNumber == 10)
+            {
+                scoreNumber = 0;
+                alert("You win! :D");
+            }
+        else
+            {
+                scoreNumber += 1;
+                score.innerHTML = scoreNumber;
+            }
+            
+    });
+});
